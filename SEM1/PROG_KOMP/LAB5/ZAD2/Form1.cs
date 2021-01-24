95f3548acc653a6ac3fb1c48f2b904157c00ef91
Pawel Alicki <pawel.alicki@gmail.com>
2021-01-24 17:06:40 +0100

    uploading solution for LAB5 exercise #2

---
+++ SEM1/PROG_KOMP/LAB5/ZAD2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LAB5_ZAD2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.ForeColor = Color.Green;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var fileContent = string.Empty;
+                var fileStream = openFileDialog1.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    fileContent = reader.ReadToEnd();
+                }
+
+                MessageBox.Show(fileContent,"Tresc pliku", MessageBoxButtons.OK);
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
